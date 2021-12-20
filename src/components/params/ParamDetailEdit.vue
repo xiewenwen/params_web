50805b60bd27b0279a605607314ec7916ffcead3
xiewenwen <xiewenwen@mchz.com.cn>
2021-12-20 19:31:31 +0800

    参数解析

---
+++ src/components/params/ParamDetailEdit.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+    <h2>{{paramName}}</h2>
+<el-form ref="form" :model="sizeForm" label-width="80px" size="mini">
+  <el-form-item label="参数名">
+    <el-input v-model="sizeForm.name"></el-input>
+  </el-form-item>
+  <el-form-item label="参数值">
+    <el-input v-model="sizeForm.value"></el-input>
+  </el-form-item>
+  <el-form-item label="是否多值">
+    <el-radio-group v-model="sizeForm.resource" size="medium">
+      <el-radio border label="是"></el-radio>
+      <el-radio border label="否"></el-radio>
+    </el-radio-group>
+  </el-form-item>
+  <el-form-item size="large">
+    <el-button type="primary" @click="onSubmit">保存</el-button>
+    <el-button>取消</el-button>
+  </el-form-item>
+</el-form>
+</div>
+</template>
+
+<script>
+export default {
+    name:"ParamDetailEdit",
+    props:['paramName,paramValue'],
+     data() {
+      return {
+        sizeForm: {
+          name: '',
+          value: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+      };
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    },
+    mounted(){
+        this.sizeForm.name=this.paramName;
+        this.sizeForm.value=this.paramValue;
+        // console.log('data is'+this.data);
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/params/ParamTemplate.vue
+++ src/components/params/ParamTemplate.vue
@@ -1,56 +1,91 @@
 <template>
+
   <div class="text-style">
+    
+    <el-button  class="btnstyle" type="primary" plain size="mini" @click="trans2"
+      >参数转换</el-button
+    >
+    <!-- <el-button   type="primary" plain size="mini" @click="getpart"
+      >解构</el-button
+    > -->
+    
     <el-input
       type="textarea"
-      :rows="8"
+      :rows="5"
       placeholder="参数模版输入"
       v-model="params"
     >
     </el-input>
-    <el-button type="primary" plain size="mini" @click="trans"
-      >参数转换</el-button
-    >
-    <el-form
-      :model="formData"
-      ref="ruleForm"
+    
+    <div v-for="(param, i) in noramlData.params" :index="param" :key="i" >
+    <el-descriptions style="height:auto;width:500px;margin-top: 10px;" class="margin-top" :column="1" size="mini" border>
+      <el-descriptions-item>
+      <template slot="label">
+        
+        参数名
+      </template>
+      {{i}}
+    </el-descriptions-item>
+    <!-- <el-descriptions-item>
+      <template slot="label">
+       
+        操作
+      </template>
+      <a>解构</a>
+    </el-descriptions-item> -->
+    <el-descriptions-item>
+      <template slot="label">
+       
+        参数值
+      </template>
+      {{param}}
+    </el-descriptions-item>   
+    </el-descriptions>
+    
+  <!-- <el-collapse style="width:500px" v-show="typeof(param)==='object'" v-model="activeNames" @change="handleChange">
+    
+  <el-collapse-item v-for="(p,i) in param" :key="i" :title="i+''" :name="i">
+    <div>{{p}}</div>
+   
+  </el-collapse-item>
+</el-collapse> -->
+<!-- <ParamDetailEdit :paramValue="param" :paramName="i"></ParamDetailEdit> -->
+    </div>
+    <!-- <el-form
+      :model="noramlData"
+      ref="noramlData"
       label-width="100px"
       class="demo-ruleForm"
       style="width: 500px"
       size="mini"
     >
-      <div v-for="(p, i) in formData.paramsList" :index="p" :key="i">
-        <!-- {{ i }}<el-input v-model="paramsList[i]"></el-input> -->
-        <div v-show="p.type === 'array'">
-          <div v-for="(pa, i) in p" :key="i">
-            <el-input v-model="p[i]"></el-input>
-            <div>{{ i }}----{{ pa }}</div>
-            <!-- <div v-show="p.type === array">
-            <div>数据类型：{{ p.type }}</div>
-            <div>{{ i }}----{{ pa }}</div>
-          </div> -->
-          </div>
-        </div>
+      <div class="paramStyle" v-for="(param, i) in noramlData.params" :index="param" :key="i">
+        
+         <div><span>{{i}}:</span>{{param}} <el-link type="primary" style="width:75px;height:14px">主要链接</el-link></div>
 
-        <!-- {{ p }} -->
       </div>
-      <!-- <div v-for="(params, i) in paramsList" :key="i">
-        <div>{{ params.type }}+{{ params[i] }}</div>
-      </div> -->
-      <el-form-item>
+  </el-form> -->
+      <!-- <el-form-item>
         <el-button type="primary" @click="submitForm('ruleForm')"
           >生成</el-button
         >
-        <!-- <el-button @click="resetForm('ruleForm')">重置</el-button> -->
-      </el-form-item>
-    </el-form>
+       
+      </el-form-item> -->
+    
+    
   </div>
 </template>
 
 <script>
 import axios from "axios";
 // import qs from "qs";
+// import ParamDetailEdit from './ParamDetailEdit.vue'
+
 export default {
   name: "ParamTemplate",
+  components:{
+    // ParamDetailEdit
+  },
   data() {
     return {
       params:
@@ -62,34 +97,49 @@ export default {
       formData: {
         paramsList: [],
       },
+      noramlData:{   
+        params: [],
+        type: "",
+        only:true
+     
+      },
+      activeNames: ['1']
     };
   },
   methods: {
-    trans() {
-      console.log("tranlate param" + JSON.stringify(this.params));
-      //   let a = this.params.replace('"', $quot);
-      axios
-        .post("/transParams", {
-          params: this.params,
-        })
-        .then(
-          (response) => {
-            this.paramsList = response.data;
-            console.log(this.paramsList);
-            for (let p in this.paramsList) {
-              console.log(p);
-              let lastp = JSON.parse(this.paramsList[p]);
-              console.log(lastp.type);
-              console.log(lastp);
-              this.formData.paramsList.unshift(lastp);
-              //   console.log(console.log(this.paramsList[p].type));
-            }
-          },
-          (error) => {
-            console.log(error);
-          }
-        );
+    trans2(){
+      console.log("trans2");
+      axios.post("/tParams",this.params,{headers: {'Content-Type': 'application/json'}}).then(
+        (response)=>{
+          console.log(response.data)
+          let data=response.data;
+          this.noramlData.params=data.paramDetail;
+          this.noramlData.type=data.type;
+          this.noramlData.only=data.only;
+          console.log('数据详情'+this.noramlData.params);
+        },
+        (error)=>{
+          console.log(error)
+
+        }
+      );
     },
+    handleChange(val) {
+        console.log(val);
+      },
+    // getpart(){
+    //    while(typeof(value)==='object'){
+
+    //     }
+      
+    //   for(let p in this.noramlData.params){
+    //     console.log(p);
+    //     let value=this.noramlData.params[p];
+       
+    //     console.log(value)
+        
+    //   }
+    // },
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
@@ -110,5 +160,14 @@ export default {
 <style>
 .text-style {
   height: auto;
+}
+.paramStyle{
+  height: 50PX;
+  width: 500px;
+}
+.btnstyle{
+  margin-top: 5%;
+  position: absolute;
+  
 }
 </style>
